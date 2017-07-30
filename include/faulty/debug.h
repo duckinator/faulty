ff4322ca5a2ee8f90c9ce6be472b2a571acd92b2
Ellen Marie Dash <me@duckie.co>
2017-07-30 19:31:44 -0400

    make debug.h work with -std=c99.

--- include/faulty/debug.h
+++ include/faulty/debug.h
@@ -2,10 +2,10 @@
 #define FAULTY_DEBUG_H
 
 #if defined(DEBUG) && DEBUG > 0
-    #define DEBUG_PRINT(fmt, args...) fprintf(stderr, "DEBUG: %s:%d:%s(): " fmt, \
-                __FILE__, __LINE__, __func__, ##args)
+    #define DEBUG_PRINT(fmt, ...) fprintf(stderr, "DEBUG: %s:%d:%s(): " fmt, \
+                __FILE__, __LINE__, __func__, ## __VA_ARGS__)
 #else
-    #define DEBUG_PRINT(fmt, args...) /* Do nothing if not in release builds. */
+    #define DEBUG_PRINT(fmt, ...) /* Do nothing if not in release builds. */
 #endif
 
 #endif
