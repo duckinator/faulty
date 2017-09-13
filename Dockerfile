f7fe8fafb8b49ec474dbe9b7bf9df781febf54f1
Ellen Marie Dash <me@duckie.co>
2017-09-12 23:45:43 -0400

    ???

--- Dockerfile
+++ Dockerfile
@@ -3,7 +3,9 @@ FROM ubuntu:16.04
 ENV USE_DEFAULT_MAKE_CONFIG true
 
 RUN apt-get update \
-    && DEBIAN_FRONTEND=noninteractive apt-get install -y build-essential git libsdl2-dev gcc-mingw-w64-x86-64 \
+    && DEBIAN_FRONTEND=noninteractive apt-get install -y build-essential git libsdl2-dev gcc-mingw-w64-x86-64 mercurial \
     && rm -rf /var/lib/apt/lists/*
+
+# RUN build-sdl2.sh
 
 ENTRYPOINT ["make"]
---
+++ cross-compiler/build-sdl2.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+function fail() {
+  echo "$@"
+  exit 1
+}
+
+PREFIX=$(pwd)
+TOOLSET=x86_64-w64-mingw32
+export CC="$TOOLSET-gcc -static-libgcc"
+
+
+mkdir $PREFIX || fail "Couldn't create $PREFIX directory."
+
+hg clone https://hg.libsdl.org/SDL SDL && cd SDL || fail "Couldn't clone SDL repository."
+
+mkdir build-win64 && cd $_ || fail "Couldn't create build-win64 directory."
+
+../configure --target=$TOOLSET --host=$TOOLSET \
+             --build=x86_64-linux --prefix=$PREFIX/$TOOLSET || fail "Failed to run ./configure for cross-compiled SDL for Windows."
+
+make || fail "Failed to build cross-compiled SDL for Windows."
+make install || fail "Failed to install cross-compiled SDL for Windows."
